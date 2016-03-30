4e0c27f56e12fa6e6380b596b8f3590690825771
Sheng-Huai Wang <m10215059@csie.ntust.edu.tw>
2016-03-31 03:46:15 +0800

    add spicy dependency

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name='finance4py',
       author_email='m10215059@csie.ntust.edu.tw',
       license='BSD',
       url='https://github.com/m10215059/finance4py/',
-      install_requires=['pandas>=0.18.0', 'pandas-datareader>=0.2.0', 'matplotlib>=1.5.1'],
+      install_requires=['pandas>=0.18.0', 'pandas-datareader>=0.2.0', 'matplotlib>=1.5.1', 'scipy'],
       packages=['finance4py'],
      )
